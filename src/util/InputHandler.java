858abd8e9209808b746c31d82d9f8acf812a2c4c
jstrong1105 <jstrong1105@naver.com>
2026-01-26 22:00:51 +0900

    Initial commit

---
+++ src/util/InputHandler.java
@@ -0,0 +1,66 @@
+package util;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+
+/**
+ * 사용자에게 입력을 받아 처리하는 도구
+ */
+public class InputHandler {
+
+    private final BufferedReader br;
+
+    public InputHandler(BufferedReader br)
+    {
+        this.br = br;
+    }
+
+    // 문자를 입력받는 메소드
+    public String readString(String prompt)
+    {
+        try
+        {
+            System.out.print(prompt + " : ");
+            return br.readLine();
+        }
+        catch (IOException e)
+        {
+            throw new RuntimeException(e);
+        }
+    }
+
+    // 숫자만 입력받는 메소드
+    public int readInt(String prompt)
+    {
+        while(true)
+        {
+            try
+            {
+                return Integer.parseInt(readString(prompt));
+            }
+            catch (NumberFormatException e)
+            {
+                System.out.println("숫자만 입력해주세요.");
+            }
+        }
+    }
+
+    // 숫자를 제한 범위 내에서 입력받는 메소드
+    public int readIntRange(String prompt, int min, int max)
+    {
+        while(true)
+        {
+            int num = readInt(prompt + " (" + min + " ~ " + max + " )");
+
+            if(num >= min && num <= max)
+            {
+                return num;
+            }
+
+            else
+            {
+                System.out.println(min + " ~ " + max + "사이로 입력해주세요.");
+            }
+        }
+    }
+}
